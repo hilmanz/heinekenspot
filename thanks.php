f92bb22365f618e0adb8373349804e63bf1a609d
Rangga Herly Hilman Herawan <rangga.hilmanz@gmail.com>
2017-07-27 15:33:15 +0700

    BACKUP
    BACKUP

---
+++ thanks.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8" />
+<meta name = "viewport" content = "width=device-width, maximum-scale = 1, minimum-scale=1" />
+<meta name="keywords" content="social media monitoring, social media monitoring indonesia,media monitoring indonesia,sentiment analysis bahasa indonesia,sonar media monitoring,sonar social media monitoring" />
+<meta name="robots" content="index,all"/>
+<link type="text/css" href="css/heineken.css" rel="stylesheet" />
+<!-- Add jQuery library -->
+<script type="text/javascript" src="js/jquery-1.10.1.min.js"></script>
+<script src="js/modernizr.js"></script>
+<script src="js/heineken.js"></script>
+<title>HEINEKEN</title>
+<!--[if (gte IE 6)&(lte IE 8)]>
+    <script type="text/javascript" src="js/selectivizr-min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+    <div id="body">
+        <a id="thanks">
+        	<img src="images/afterlogin.png" />
+        </a><!-- end #thanks -->
+    </div><!-- end #body -->
+</body>
+</html>
